b3d336db228940a7fce5c013c76cf23b711d1818
Bastián Jara Giuffra <bastian.jara14@gmail.com>
2018-10-23 11:42:34 -0300

    bd modificada

--- build/web/WEB-INF/classes/bd/bd_ws.sql
+++ build/web/WEB-INF/classes/bd/bd_ws.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-09-2018 a las 04:07:10
+-- Tiempo de generación: 23-10-2018 a las 14:40:22
 -- Versión del servidor: 5.7.14
 -- Versión de PHP: 5.6.25
 
@@ -19,6 +19,17 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `bd_ws`
 --
+
+DELIMITER $$
+--
+-- Procedimientos
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `listarMultasByRut` (IN `p_rut` VARCHAR(15))  BEGIN
+  SELECT * FROM MULTAS
+  WHERE rut = p_rut;
+END$$
+
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -40,7 +51,10 @@ INSERT INTO `aeronaves` (`id`, `patente`, `fecha_aeronavegabilidad`) VALUES
 (1, 'CC-WBS', '2018-09-25 20:34:44'),
 (2, 'CC-JPG', '2018-09-25 20:37:51'),
 (3, 'CC-CHU', '2018-09-25 20:37:51'),
-(4, 'CC-PA', '2018-09-25 20:37:51');
+(4, 'CC-PA', '2018-09-25 20:37:51'),
+(5, 'CC-HPF', '2018-09-30 23:07:38'),
+(6, 'CC-IIII', '2018-10-14 17:58:17'),
+(7, 'CC-IIIA', '2018-10-14 17:58:43');
 
 -- --------------------------------------------------------
 
@@ -61,7 +75,30 @@ CREATE TABLE `licencias` (
 
 INSERT INTO `licencias` (`id`, `rut`, `nro_licencia`, `tipo_aeronave`) VALUES
 (1, '19516416-9', 456445, 'Avion'),
-(2, '16544449-3', 456654, 'Helicoptero');
+(2, '16544449-3', 456654, 'Helicoptero'),
+(3, '19516416-9', 666, 'Helicóptero');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `medicinas`
+--
+
+CREATE TABLE `medicinas` (
+  `id` int(10) NOT NULL,
+  `descripcion` varchar(255) NOT NULL,
+  `fecha_vcto` timestamp NOT NULL,
+  `rut` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `medicinas`
+--
+
+INSERT INTO `medicinas` (`id`, `descripcion`, `fecha_vcto`, `rut`) VALUES
+(1, 'Paracetamol', '2018-10-22 22:36:36', '19516416-9');
+INSERT INTO `medicinas` (`id`, `descripcion`, `fecha_vcto`, `rut`) VALUES
+(2, 'Ibuprofeno', '2018-10-23 22:36:36', '16544449-3');
 
 -- --------------------------------------------------------
 
@@ -103,6 +140,12 @@ ALTER TABLE `licencias`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `medicinas`
+--
+ALTER TABLE `medicinas`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `multas`
 --
 ALTER TABLE `multas`
@@ -116,12 +159,17 @@ ALTER TABLE `multas`
 -- AUTO_INCREMENT de la tabla `aeronaves`
 --
 ALTER TABLE `aeronaves`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT de la tabla `licencias`
 --
 ALTER TABLE `licencias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT de la tabla `medicinas`
+--
+ALTER TABLE `medicinas`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT de la tabla `multas`
 --
